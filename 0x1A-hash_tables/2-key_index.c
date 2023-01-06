c3f88e40bcd4ebe858b7be92652e9b5d88af1b75
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-06 19:14:32 +0100

    commit

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/*
+ *
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *key++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash % size);
+}
