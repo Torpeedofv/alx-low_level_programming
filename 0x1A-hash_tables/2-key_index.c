70dba16e40ad6dc74bcb650045d5e7cfd084cfe2
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-06 19:19:32 +0100

    commit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
-/*
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
  *
+ * Return: index at which the key or value will be stored
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
