5e6a896ac9ee2df362e2c8f9fd3f6385ab50e777
Torpeedofv <torpeedo.fv@gmail.com>
2023-01-07 12:09:43 +0100

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,11 +15,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newNode, *temp;
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	unsigned long int index;
 
 	if (ht == NULL || strlen(key) == 0 || key == NULL)
 		return (0);
 
+	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
 	while (temp)
