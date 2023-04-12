bf80ef78b55e2a514dc95d6e7b3af1fcab1eca28
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-12 12:16:27 +0100

    commit for task 3

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,6 +13,8 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t start = 0, end = size - 1, mid, i;
 
+	if (array == NULL)
+		return (-1);
 	while (start <= end)
 	{
 		mid = (start + end) / 2;
